569a3d0421f6d2dda2b52d29960687d18ef459eb
MarkOfTriumph <marco.ganesio@gmail.com>
2023-12-14 11:47:45 +0100

    Versione Finale

--- src/org/generation/italy/model/GestoreUtenti.java
+++ src/org/generation/italy/model/GestoreUtenti.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 public class GestoreUtenti {
 	ArrayList<Account> elencoAccount;
-	GestoreUtenti()
+	
+	
+	//costruttore
+	public GestoreUtenti()
 	{
 	 elencoAccount= new ArrayList<>();
 	}
@@ -13,20 +16,49 @@ public class GestoreUtenti {
 	
 	public boolean aggiungiUtente(Account acc)
 	{
+		
 		if (!elencoAccount.contains(acc))
 		{
 			elencoAccount.add(acc);
 			return true;
-		}	
-		else 
+		}	 
 			return false;
 	}
 	
 	public boolean rimuoviUtente(String username)
 	{
 		for(int i=0;i<elencoAccount.size();i++)
-			if(elencoAccount[i].)
+		{
+			if(elencoAccount.get(i).getUsername().equals(username))
+			{
+				elencoAccount.remove(i);
+				return true;
+			}
+		}
+		return false;
 	}
+	
+	public Account loginUtente(String username, String password)
+	{
+		for(int i=0;i<elencoAccount.size();i++)
+			if (elencoAccount.get(i).getUsername().equals(username) && elencoAccount.get(i).getPassword().equals(password))
+				return elencoAccount.get(i) ;
+		return null; 
+	}
+	
+	public boolean resettaPassword(String username)
+	{
+		for(int i=0;i<elencoAccount.size();i++)
+		{
+			if(elencoAccount.get(i).getUsername().equals(username))
+			{
+				elencoAccount.get(i).setPassword("12345");
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	
 	
 }
